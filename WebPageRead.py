529ef407bdc1bd95587ccba96ba9f00aad6b115e
theoho-png <theodoraho10@gmail.com>
2020-03-11 21:17:36 +1100

    Import Lib

---
+++ WebPageRead.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import webbrowser
+from pypi import selenium
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+print("Input Link")
+#url = input()
+
+f = open("https://www.w3schools.com/python/python_file_handling.asp", "r")
+print (f)
+
+#f.close
+
+#soup = BeautifulSoup(html_doc, 'html.parser')
+#print(soup.prettify())
