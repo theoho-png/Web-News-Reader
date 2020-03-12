c14eaf4f52ec046249054f6781975bba90c0a0f2
theoho-png <theodoraho10@gmail.com>
2020-03-12 16:25:20 +1100

    Developing reader into a class.

--- WebPageRead.py
+++ WebPageRead.py
@@ -3,25 +3,31 @@ import urllib.request
 import urllib.parse
 import re
 
-#url = input()
-url = "https://www.abc.net.au/news/2020-03-11/coronavirus-stimulus-package-to-include-billions-for-apprentices/12046688"
-values = {'s':'basics', 'submit':'search'}
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-resp = urllib.request.urlopen(req)
-respData = resp.read()
-#https://www.youtube.com/watch?v=GEshegZzt3M
-htmlDoc = str(respData)
+#url = "https://www.abc.net.au/news/2020-03-11/coronavirus-stimulus-package-to-include-billions-for-apprentices/12046688"
 
-soup = BeautifulSoup(htmlDoc, 'html.parser')
-soup = soup.find("div", class_="article section")
+class Reader:
 
-container = ""
-filterList = [',', '.', '$', '\\', '/'', ''"']
+    def loadUrl(self):
+        values = {'s':'basics', 'submit':'search'}
+        data = urllib.parse.urlencode(values)
+        data = data.encode('utf-8')
+        req = urllib.request.Request(url, data)
+        resp = urllib.request.urlopen(req)
+        #https://www.youtube.com/watch?v=GEshegZzt3M
+        return str(resp.read())
 
-for passage in soup.find_all("p", class_=""):
-    container += passage.text
-    container = re.sub(r'[^\w]', ' ', container)
+    def finder(self):
+        data = ""
+        content = soup.find("div", class_="article section")
+        for passage in content.find_all("p", class_=""):
+            data += passage.text
+            data = re.sub(r'[^\w]', ' ', data)
+        return data
 
-print (container)
+if __name__ == "__main__":
+    print("Pleas input news link.")
+    url = input()
+    r = Reader()
+    soup = BeautifulSoup(r.loadUrl(), 'html.parser')
+    
+    print(r.finder())
