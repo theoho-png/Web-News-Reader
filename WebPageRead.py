8d2ef68da8cc3df78a7c9da0657ec71d32f57552
theoho-png <theodoraho10@gmail.com>
2020-03-12 11:30:20 +1100

    Read Web Page

--- WebPageRead.py
+++ WebPageRead.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
-import re
-
 
 #url = input()
 url = "https://www.abc.net.au/news/2020-03-11/coronavirus-stimulus-package-to-include-billions-for-apprentices/12046688"
@@ -11,15 +9,10 @@ data = urllib.parse.urlencode(values)
 data = data.encode('utf-8')
 req = urllib.request.Request(url, data)
 resp = urllib.request.urlopen(req)
-
 respData = resp.read()
-paragraphs = re.findall(r'<p>(.*?)</p>', str(respData))
-for eachP in paragraphs:
-    print (eachP)
-#https://www.youtube.com/watch?v=GEshegZzt3M """
+#https://www.youtube.com/watch?v=GEshegZzt3M
 
+htmlDoc = str(respData)
 
-#else:
-#print("Unable to optain web page")
-
-#with open() as urlFile:
+soup = BeautifulSoup(htmlDoc, 'html.parser')
+print(soup.prettify())
