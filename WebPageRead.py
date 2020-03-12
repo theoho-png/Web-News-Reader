a5c1c2d0f7d3fa1761402cf95a46e63b99c68e19
theoho-png <theodoraho10@gmail.com>
2020-03-12 17:10:51 +1100

    Added topic finder in Reader class.

--- WebPageRead.py
+++ WebPageRead.py
@@ -13,14 +13,18 @@ class Reader:
         resp = urllib.request.urlopen(req)
         #https://www.youtube.com/watch?v=GEshegZzt3M
         return str(resp.read())
-
-    def finder(self):
+    
+    def pFinder(self):
         data = ""
         content = soup.find("div", class_="article section")
         for passage in content.find_all("p", class_=""):
             data += passage.text
             data = re.sub(r'[^\w]', ' ', data)
         return data
+
+    def topicFinder(self):
+        content = soup.find("div", class_="header subheader")
+        return content.find('h1').get_text()
 
 if __name__ == "__main__":
     print("Pleas input news link.")
@@ -29,6 +33,7 @@ if __name__ == "__main__":
     if (link.status == 200):
         r = Reader()
         soup = BeautifulSoup(r.loadUrl(), 'html.parser')
-        print(r.finder())
+        words = r.pFinder().split()
+        print(r.topicFinder())
     else:
         print ("Unable to open web page.")
